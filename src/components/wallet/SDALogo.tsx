6ed1f288062aa669461a1aa5b33eaa8dc48f1326
Liman5 <limanseidoucairos9@gmail.com>
2026-03-14 17:07:11 +0000

    Switch SDA logo from external URL to local hosted image

--- src/components/wallet/SDALogo.tsx
+++ src/components/wallet/SDALogo.tsx
@@ -22,7 +22,7 @@ export function SDALogo({ size = 'md', className, showLabel = false }: SDALogoPr
     <div className={cn('flex items-center gap-1.5', className)}>
       <div className="relative" style={{ width: pixelSize, height: pixelSize }}>
         <Image
-          src="https://image2url.com/r2/default/images/1772288434807-f6ee6c1e-ae55-4647-8dc2-032b697c4bf9.png"
+          src="/images/sda-logo.png"
           alt="SDA Token Logo"
           fill
           className="object-contain rounded-full"
@@ -41,7 +41,7 @@ export function SDALogoInline({ size = 'sm', className }: Omit<SDALogoProps, 'sh
   return (
     <div className={cn('inline-flex items-center', className)} style={{ width: pixelSize, height: pixelSize }}>
       <Image
-        src="https://image2url.com/r2/default/images/1772288434807-f6ee6c1e-ae55-4647-8dc2-032b697c4bf9.png"
+        src="/images/sda-logo.png"
         alt="SDA Token Logo"
         width={pixelSize}
         height={pixelSize}
